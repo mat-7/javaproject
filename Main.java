6cd6e711111f3d4d3ffe55f9706c77c569e3bda6
mat-7 <70146826+mat-7@users.noreply.github.com>
2020-10-16 16:10:19 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,43 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (sc.hasNext()){
-        for (int i = 0; i < T; i++){
-             int n = sc.nextInt();
-             int m = sc.nextInt();
-             int k = sc.nextInt();
-             int[][] arr = new int[m][3];
-             for (int j = 0; j < m; j++){
-                 arr[j][0] = sc.nextInt();
-                 arr[j][1] = sc.nextInt();
-                 arr[j][2] = sc.nextInt();
-             }
-             if (bfs(arr, n, m, k)) System.out.println("Yes");
-             else System.out.println("No");
-        }
-        }
-    }
+        int m = sc.nextInt();
+        int n = sc.nextInt();
 
-    public static boolean bfs(int[][] arr, int n, int m, int k){
-        Set<Integer> set = new HashSet<>();
+        char[][] matrix = new char[m][n];
         for (int i = 0; i < m; i++){
-            if (arr[i][2] <= k){
-                set.add(arr[i][0]);
-                set.add(arr[i][1]);
+            String temp = sc.next();
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = temp.charAt(j);
             }
         }
-
-        return set.size() == n;
+        System.out.println(maximalSquare(matrix));
     }
+    
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == 'M') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+
+
 }
